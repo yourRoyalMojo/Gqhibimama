d5e7f66fbd4358debd5c5d07d69bd871553789dc
Mojane Sekhutsoanyane <msekhutsoanyaneinc025@student.wethinkcode.co.za>
2025-08-28 17:15:04 +0200

    There are bugs in this house. Damn Roaches.

--- src/main/java/com/mojaneworks/Turn.java
+++ src/main/java/com/mojaneworks/Turn.java
@@ -33,28 +33,35 @@ public class Turn {
 
             System.out.println("No cards on the table. 'toss' a card in your hand.");
             System.out.println("e.g '2 Spades'");
-            String correctInput = input.nextLine();
+            System.out.println("Your hand: ");
+            for (Card card : currentPlayer.getPlayerHand().getHandCards()){
 
+                System.out.println(card.getCardNumber() + " of " + card.getCardSuit());
+            }
+
+            String correctInput = input.nextLine();
+            ArrayList<Card> playerHand = currentPlayer.getPlayerHand().getHandCards();
             while (true) {
 
                 String cardSuit = correctInput.split(" ")[1];
                 String cardNumber = correctInput.split(" ")[0];
 
 
-                    for (Card card : currentPlayer.getPlayerHand().getHandCards()) {
+                for (int i = 0; i < playerHand.size(); i++) {
 
-                        if (card.getCardNumber() == Integer.parseInt(cardNumber) &&
-                                card.getCardSuit().equalsIgnoreCase(cardSuit)) {
-                            System.out.println("Tossing the " +
-                                card.getCardNumber() + " of " + card.getCardSuit());
-                            currentPlayer.getPlayerHand().getHandCards().remove(card);
-                            this.tableCards.add(card);
-                            break;
-                        }
-                        else{
-                            System.out.println("card not in your hand. choose a card in your hand");
-                            correctInput = input.nextLine();
-                        }
+                    if (playerHand.get(i).getCardNumber() == Integer.parseInt(cardNumber) &&
+                            playerHand.get(i).getCardSuit().equalsIgnoreCase(cardSuit)) {
+                        System.out.println("Tossing the " +
+                                playerHand.get(i).getCardNumber() + " of " + playerHand.get(i).getCardSuit());
+                        currentPlayer.getPlayerHand().getHandCards().remove(playerHand.get(i));
+                        this.tableCards.add(playerHand.get(i));
+                        break;
+                    }
+                    else if(playerHand.get(i).getCardNumber() != Integer.parseInt(cardNumber) &&
+                            !playerHand.get(i).getCardSuit().equalsIgnoreCase(cardSuit)){
+                        System.out.println("card not in your hand. choose a card in your hand");
+                        correctInput = input.nextLine();
+                    }
                 }
             }
         }
