35f59827e4959fa5e0d6f031d2fc2798d579738f
Mojane Sekhutsoanyane <msekhutsoanyaneinc025@student.wethinkcode.co.za>
2025-08-31 09:53:49 +0200

    Pest control is crucial. Can't worry about structural integrity and bugs.

--- src/main/java/com/mojaneworks/Turn.java
+++ src/main/java/com/mojaneworks/Turn.java
@@ -18,8 +18,14 @@ public class Turn {
     public void takeTurn(){
 
         Scanner input = new Scanner(System.in);
+        System.out.println("Player " + (currentPlayer.getPlayerNumber() + 1) + "'s turn\n");
         System.out.println("enter 'build' or 'toss'");
         String playerInput = input.nextLine();
+
+        while (!playerInput.equalsIgnoreCase("build") && !playerInput.equalsIgnoreCase("toss")) {
+            System.out.println("Choose between 'build' and 'toss' please");
+            playerInput = input.nextLine();
+        }
 
         if (!this.tableCards.isEmpty() && playerInput.equalsIgnoreCase("build")){
             System.out.println("Choose a card from the following: ");
@@ -43,6 +49,7 @@ public class Turn {
             ArrayList<Card> playerHand = currentPlayer.getPlayerHand().getHandCards();
             while (true) {
 
+                boolean cardFound = false;
                 String cardSuit = correctInput.split(" ")[1];
                 String cardNumber = correctInput.split(" ")[0];
 
@@ -53,15 +60,19 @@ public class Turn {
                             playerHand.get(i).getCardSuit().equalsIgnoreCase(cardSuit)) {
                         System.out.println("Tossing the " +
                                 playerHand.get(i).getCardNumber() + " of " + playerHand.get(i).getCardSuit());
-                        currentPlayer.getPlayerHand().getHandCards().remove(playerHand.get(i));
                         this.tableCards.add(playerHand.get(i));
-                        break;
+                        currentPlayer.getPlayerHand().getHandCards().remove(playerHand.get(i));
+                        cardFound = true;
                     }
-                    else if(playerHand.get(i).getCardNumber() != Integer.parseInt(cardNumber) &&
-                            !playerHand.get(i).getCardSuit().equalsIgnoreCase(cardSuit)){
-                        System.out.println("card not in your hand. choose a card in your hand");
-                        correctInput = input.nextLine();
-                    }
+                }
+                if(!cardFound){
+                    System.out.println("card not in your hand. choose a card in your hand");
+                    System.out.println("i'm here");
+                    correctInput = input.nextLine();
+                }
+                else{
+
+                    break;
                 }
             }
         }
